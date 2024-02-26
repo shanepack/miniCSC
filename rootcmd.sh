200d9d5a5fec3b661bb46ed7b514f66b478936b1
shanepack <shanepack00@gmail.com>
2024-02-26 12:45:14 +0100

    Changed directory

---
+++ rootcmd.sh
@@ -0,0 +1,6 @@
+#To automatically run this script, navigate to ~/rootcode (cd ~/rootcode in terminal)
+#and then type into the terminal: ./rootcmd.sh
+
+cd ~
+. root-6.30.04-install/bin/thisroot.sh
+root ~/rootcode/miniCSC/plotFluka.cpp
