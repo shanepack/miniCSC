01db619817652e5e7d6eac9e70e8e6a657c2524c
Dylan Parks <dylan@bouwerbytes.com>
2024-03-07 16:54:42 +0100

    Added command line arguments
    Also began working on HTML format styling.

--- tmb_dump_script.py
+++ tmb_dump_script.py
@@ -1,5 +1,16 @@
 import os
+import argparse
 import json
+
+VERBOSE = False
+HTML = False
+
+
+def log(*format: str):
+    if VERBOSE:
+        for msg in format:
+            print(msg, end=" ")
+        print()
 
 
 def parse_txt(file_path: str, header_data, tmb_data):
@@ -54,6 +65,7 @@ def get_run_num(file):
     return int(file.split("-")[0][1:])
 
 
+# TODO: doing some funky stuff to get the right data
 def generate_elog(files: list[str]):
     buffer = ""
     # Initialize data structure
@@ -68,12 +80,23 @@ def generate_elog(files: list[str]):
         "20CLCT": 0,
         "32TMB": 0,
     }
+    if HTML:
+        log("Inserting HTML styling")
+        buffer += (
+            '<pre>\n<strong><span style="font-size:large">Title\n\n</span></strong>'
+        )
     for file in files:
         file_data = parse_txt(file, header_data, tmb_data)
-        buffer += f"File: {file}\n"
-        for key, value in file_data.items():
-            buffer += f"{key}: {value}\n"
-        buffer += "\n"
+        # for key, value in file_data.items():
+        #     buffer += f"{key}: {value}\n"
+        buffer += "Start: " + file_data["Start"] + "\n"
+        buffer += "Stop: " + file_data["Stop"] + "\n"
+        buffer += "0ALCT: " + str(file_data["0ALCT"]) + "\n"
+        buffer += "20CLCT: " + str(file_data["20CLCT"]) + "\n"
+        buffer += "32TMB: " + str(file_data["32TMB"]) + "\n"
+
+    buffer += "</pre>"
+    # buffer += "</span></strong></pre>"
     return buffer
 
 
@@ -108,22 +131,50 @@ def generate_csv(files: list[str]):
     return buffer
 
 
-def process_directory(directory):
+def process_directory(directory: str, num: int):
+    log(f"Processing {num} files from: {directory}")
     files = os.listdir(directory)
     new_files = []
+    count = 0
     for file in files:
         if (
             file.split("-")[0][1:].isdigit()
             and file.endswith(".txt")
             and file.startswith("r")
         ):
-            print(f"Will process {file}")
+            if count >= num:
+                break
+            count += 1
+            log(f"Will process {file}")
             new_files.append(file)
 
     return sorted(new_files, key=get_run_num)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Increase script verbosity"
+    )
+    # parser.add_argument(
+    #     "-e", "--elog", action="store_true", help="Generate ELOG output"
+    # )
+    # parser.add_argument("-c", "--csv", action="store_true", help="Generate CSV output")
+    parser.add_argument(
+        "-n",
+        "--num",
+        type=int,
+        default=100,
+        metavar="num",
+        help="Specify number of files to process (default: 100)",
+    )
+    parser.add_argument("--html", action="store_true", help="Export with HTML styling")
+    args = parser.parse_args()
+    VERBOSE = args.verbose
+    # ELOG = args.elog
+    HTML = args.html
+    # CSV = args.csv
+
     # Update these paths according to your local setup
     directory = os.path.dirname(__file__)
     output_dir = os.path.join(directory, "output")
@@ -132,17 +183,20 @@ if __name__ == "__main__":
     elog_out = os.path.join(output_dir, "elog_out.txt")
     csv_out = os.path.join(output_dir, "csv_out.txt")
 
-    files = process_directory(directory)
+    log(f"Parsing directory: {directory}")
+    files = process_directory(directory, args.num)
 
     # Generate elog
-    print("Generating Elog at: ", elog_out)
+    # if ELOG:
+    log("Generating Elog at: ", elog_out)
     elog_data = generate_elog(files)
     elog_file = open(elog_out, "w")
     elog_file.write(elog_data)
     elog_file.close()
 
     # Generate CSV
-    print("Generating CSV at: ", csv_out)
+    # if CSV:
+    log("Generating CSV at: ", csv_out)
     csv_data = generate_csv(files)
     csv_file = open(csv_out, "w")
     csv_file.write(csv_data)
